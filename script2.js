e6b1f09da89513e2c9811981e611bef032ce25dd
desafinadude <meerkdir@gmail.com>
2024-12-03 11:31:59 +0200

    embed fix

--- script2.js
+++ script2.js
@@ -208,29 +208,29 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // END SCROLLY. UNSTICK
 
-        ScrollTrigger.create({
-            trigger: '.scrolly-section[data-section-label="end"]',
-            start: () => {
-                const backgroundRect = background.getBoundingClientRect();
-                return `top ${offsetInPixels}px`;
-            },
-            onEnter: () => {
+        // ScrollTrigger.create({
+        //     trigger: '.scrolly-section[data-section-label="end"]',
+        //     start: () => {
+        //         const backgroundRect = background.getBoundingClientRect();
+        //         return `top ${offsetInPixels}px`;
+        //     },
+        //     onEnter: () => {
 
-                gsap.to('.scrolly-background', {
-                    position: 'relative',
-                    duration: 0.5,
-                    ease: 'power1.inOut'
-                });
-            },
-            onLeaveBack: () => {
+        //         gsap.to('.scrolly-background', {
+        //             position: 'relative',
+        //             duration: 0.5,
+        //             ease: 'power1.inOut'
+        //         });
+        //     },
+        //     onLeaveBack: () => {
 
-                gsap.to('.scrolly-background', {
-                    position: 'sticky',
-                    duration: 0.5,
-                    ease: 'power1.inOut'
-                });
-            }
-        });
+        //         gsap.to('.scrolly-background', {
+        //             position: 'sticky',
+        //             duration: 0.5,
+        //             ease: 'power1.inOut'
+        //         });
+        //     }
+        // });
 
         // SCROLLY CAPTIONS FADE INS
 
